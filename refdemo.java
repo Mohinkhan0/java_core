545fd7e714ee3a476c8c64cc2f853af83f6adbe1
Mohin khhan <117769740+Mohinkhan0@users.noreply.github.com>
2022-11-26 08:51:04 -0800

    Add files via upload

---
+++ refdemo.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+//import java.util.Scanner;
+
+class Refdemo
+{  
+    public static void main(String[] args) throws IOException
+    {
+        // Taking input in java 
+        // it will return ASCII of the character
+        System.out.println("enter name");
+        int c= System.in.read();
+        System.out.println("hallow  " + c);  // it will print first letter of name ASCII value
+    //    System.out.println("enter your name");
+     //   Scanner sc=new Scanner(System.in);
+      //  String name=sc.nextLine();
+      //  System.out.println("hello  " + name);
+       // sc.close();
+
+    }
+}
