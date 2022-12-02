b70a2fdfa9dfebdfce64a663186133a8fec9e4ec
Mohin khhan <117769740+Mohinkhan0@users.noreply.github.com>
2022-12-02 08:22:32 -0800

    Add files via upload

---
+++ lengthfind.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class lengthfind
+{
+ public static void main(String[] args)
+  {
+ Scanner sc=new Scanner(System.in);
+ System.out.println(" enter any number ");
+ String str1=sc.next();
+  System.out.println("length is" + str1.length());
+ }
+}
